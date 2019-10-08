722d4c04d86be81d979e60d6449cb08937f990f4
AvgoustisW <avgoustisw@gmail.com>
2019-10-08 20:06:49 +0300

    Added play song functionality with AmplitudeJS - more works needs to be done in fetching mp3 files as it is currently running with a placeholder

--- src/components/tracks/Playsong.js
+++ src/components/tracks/Playsong.js
@@ -1,74 +1,69 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Consumer } from '../../context';
+import React, { Component } from 'react'  
+import Amplitude from 'amplitudejs' 
 
-class Playsong extends Component {
-  state = {
-    trackTitle: ''
-  };
+class Playsong extends Component {  
 
-  findTrack = (dispatch, e) => {
-    e.preventDefault();
+    constructor(props) {
+        super(props);
+        this.progressNav = this.progressNav.bind(this);
+    }
 
-    axios
-      .get(
-        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=${
-          this.state.trackTitle
-        }&page_size=10&page=1&s_track_rating=desc&apikey=${
-          process.env.REACT_APP_KEY
-        }`
-      )
-      .then(res => {
-        console.log(res.data)
-        dispatch({
-          type: 'SEARCH_TRACKS',
-          payload: res.data.message.body.track_list
-        });
+    componentDidMount(){
 
-        this.setState({ trackTitle: '' });
-      })
-      .catch(err => console.log(err));
-  };
+        Amplitude.init({ 
+            "bindings": {
+              37: 'prev',
+              39: 'next',
+              32: 'play_pause'
+            },
+            "songs": [
+              {
+                "name": "placeholder - Risin' High (feat Raashan Ahmad)",
+                "artist": "placeholder - Ancient Astronauts",
+                "album": "placeholder - We Are to Answer",
+                // SET PLACEHOLDER URL on placeholder mp3, this needs to be changed depending on our mp3 source 
+                // find song on open source library - > get mp3 with api - similar to the one that is used to retrieve the lyrics
+                // or a web crawler perhaps
+                "url": "https://521dimensions.com/song/Ancient%20Astronauts%20-%20Risin'%20High%20(feat%20Raashan%20Ahmad).mp3",                
+                "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg"  
+              }
+            ]
+          });       
+    }   
+    
+    progressNav(e) { 
+        const box = e.target;
+        const offset = box.getBoundingClientRect();
+        const x = e.pageX - offset.left;      
+        Amplitude.setSongPlayedPercentage( ( parseFloat( x ) / parseFloat( box.offsetWidth) ) * 100 ); 
+       
+    }
 
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  render() {
-    return (
-      <Consumer>
-        {value => {
-          const { dispatch } = value;
-          return (
-            <div className="card card-body mb-4 p-4">
-              <h1 className="display-4 text-center">
-                <i className="fas fa-music" /> Search For A Song
-              </h1>
-              <p className="lead text-center">Get the lyrics for any song</p>
-              <form onSubmit={this.findTrack.bind(this, dispatch)}>
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control form-control-lg"
-                    placeholder="Song title..."
-                    name="trackTitle"
-                    value={this.state.trackTitle}
-                    onChange={this.onChange}
-                  />
+  render() {   
+      return (
+        <React.Fragment>   
+            <div className="bottom-container">
+                <progress className="amplitude-song-played-progress" id="song-played-progress" onClick={this.progressNav}></progress>
+                <div className="time-container">
+                    <span className="current-time">
+                    <span className="amplitude-current-minutes"></span>:<span className="amplitude-current-seconds"></span>
+                    </span>
+                    <span className="duration">
+                    <span className="amplitude-duration-minutes"></span>:<span className="amplitude-duration-seconds"></span>
+                    </span>
                 </div>
-                <button
-                  className="btn btn-primary btn-lg btn-block mb-5"
-                  type="submit"
-                >
-                  Get Track Lyrics
-                </button>
-              </form>
-            </div>
-          );
-        }}
-      </Consumer>
-    );
+                <div className="control-container">
+                    <div className="amplitude-play-pause" id="play-pause"></div>
+                    <div className="meta-container">
+                    <span data-amplitude-song-info="name" className="song-name"></span>
+                    <span data-amplitude-song-info="artist"></span>
+                    </div>
+                </div>
+            </div> 
+        </React.Fragment>
+    )
   }
+
 }
 
 export default Playsong;
